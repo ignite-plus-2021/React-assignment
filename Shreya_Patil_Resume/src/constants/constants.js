a337bad4c2dacb44101f1b6d35ed8b22d9c47bd5
shreya-dotcom <77908208+shreya-dotcom@users.noreply.github.com>
2021-06-28 14:55:54 +0530

    Add files via upload

---
+++ Shreya_Patil_Resume/src/constants/constants.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    title: 'Gym Management System',
+    description: "To design Gym Management System for user",
+    // image: '/images/1.png',
+    id: 0,
+  },
+  
+  {
+    title: 'COVID-19 Record Management and Tracking',
+    description: "To design an application for covid-19 tracking and record management system",
+    // image: '/images/4.jpg',
+    id: 1,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
