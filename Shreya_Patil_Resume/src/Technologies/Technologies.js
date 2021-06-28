cdb19f217fceb9e9f2b662882b49b47869995e0c
shreya-dotcom <77908208+shreya-dotcom@users.noreply.github.com>
2021-06-28 14:48:03 +0530

    Add files via upload

---
+++ Shreya_Patil_Resume/src/Technologies/Technologies.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () =>  (
+  <Section id="tech">
+    <SectionDivider/>
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I have worked on java and data structure and algorithms.<br/>
+      Currently working on web development.
+    </SectionText>
+  </Section>
+);
+
+export default Technologies;
