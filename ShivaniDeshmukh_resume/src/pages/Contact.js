fa9f8ec7ea4e6447552dc8dc48a43f3d9a86c38a
DeshmukhShivani <42211102+DeshmukhShivani@users.noreply.github.com>
2021-07-03 00:16:20 +0530

    Create Contact.js

---
+++ ShivaniDeshmukh_resume/src/pages/Contact.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import {GrLinkedin} from 'react-icons/gr'
+
+const ContactStyles = styled.div`
+    background-color:LightGrey;
+    .col1{
+        margin:0 1 rem;
+        padding:20px 0;
+        
+    }
+    .ln
+    {
+        color:#49a1eb;
+    }
+`;
+const Contact = () => {
+    return (
+        <ContactStyles>
+            <div className="container">
+
+                
+                <div className="col1">
+                    <h3>Contact details</h3>
+                    <p>Email id:shivani.deshmukh@cumminscollege.in |<br></br>
+                        Contact No.:7378736005  </p>
+                    <a href="https://www.linkedin.com/in/shivani-deshmukh-483b6618b" className="ln" >
+                    <GrLinkedin/>
+                    </a>
+                    
+                </div>
+                
+            </div>
+        </ContactStyles>
+    )
+}
+export default Contact;
